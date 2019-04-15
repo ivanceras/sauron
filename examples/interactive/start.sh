b77249939aa3d9660006326b0d0cef2eba2ac5b4
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-15 22:13:05 +0800

    Add bootstraping script when dependencies are not installed

--- examples/interactive/start.sh
+++ examples/interactive/start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 set -v
+
+if ! type wasm-pack > /dev/null; then
+    echo "wasm-pack is not installed"
+    cargo install wasm-pack
+fi
+
+if ! type basic-http-server > /dev/null; then
+    echo "basic-http-server is not installed"
+    cargo install basic-http-server
+fi
+
+
 wasm-pack build --target no-modules
 
 basic-http-server ./ -a 0.0.0.0:5000
