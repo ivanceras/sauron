a48c852014d8bdacd3215c4f3c2187b13f1339bd
Jovansonlee Cesar <ivanceras@gmail.com>
2019-05-08 17:59:13 +0800

    Add fetch api in Program to provide functionality of fetching records from a rest API

--- src/program.rs
+++ src/program.rs
@@ -3,8 +3,11 @@ use crate::{Component,
             DomUpdater};
 use std::{cell::RefCell,
           rc::Rc};
-use wasm_bindgen::closure::Closure;
-use web_sys::Node;
+use wasm_bindgen::{closure::Closure,
+                   JsCast,
+                   JsValue};
+use web_sys::{Node,
+              Response};
 
 /// Holds the app and the dom updater
 /// This is passed into the event listener and the dispatch program
@@ -68,6 +71,75 @@ impl<APP, MSG> Program<APP, MSG>
         self.dom_updater.borrow_mut().update(self, view);
         //let t4 = crate::now();
         //crate::log!("dom update took: {}ms", t4 - t3);
+    }
+
+    /// Fetch from the url with decoder for the text response and the fail callback
+    pub fn fetch_with_text_response_decoder<DE, CB, ERR, OUT>(self: &Rc<Self>,
+                                                              url: &str,
+                                                              response_text_decoder: DE,
+                                                              cb: CB,
+                                                              fail_cb: ERR)
+        where CB: Fn(OUT) -> MSG + 'static,
+              ERR: Fn(JsValue) -> MSG + Clone + 'static,
+              DE: Fn(String) -> OUT + 'static,
+              OUT: 'static
+    {
+        let response_decoder = move |js_value: JsValue| {
+            let response_text =
+                js_value.as_string().expect("There's no string value");
+            let grouped_window = response_text_decoder(response_text);
+            cb(grouped_window)
+        };
+        self.fetch_with_response_decoder(url, response_decoder, fail_cb);
+    }
+
+    /// API for fetching http rest request
+    pub fn fetch_with_response_decoder<F, ERR>(self: &Rc<Self>,
+                                               url: &str,
+                                               response_decoder: F,
+                                               fail_cb: ERR)
+        where F: Fn(JsValue) -> MSG + 'static,
+              ERR: Fn(JsValue) -> MSG + Clone + 'static
+    {
+        let program_clone = Rc::clone(self);
+
+        let decoder_and_dispatcher = move |js_value: JsValue| {
+            let msg = response_decoder(js_value);
+            program_clone.dispatch(msg);
+        };
+
+        let program_clone_status_err = Rc::clone(self);
+        let fail_status_cb = fail_cb.clone();
+
+        let promise = crate::window().fetch_with_str(url);
+        let cb: Closure<FnMut(JsValue)> =
+            Closure::once(move |js_value: JsValue| {
+                let response: &Response = js_value.as_ref().unchecked_ref();
+                let status = response.status();
+                crate::log!("status: {}", status);
+                if status == 200 {
+                    let response_promise =
+                        response.text().expect("expecting a text");
+                    let decoder_and_dispatcher_cb: Closure<FnMut(JsValue)> =
+                        Closure::once(decoder_and_dispatcher);
+                    response_promise.then(&decoder_and_dispatcher_cb);
+                    decoder_and_dispatcher_cb.forget();
+                } else {
+                    program_clone_status_err.dispatch(fail_status_cb(js_value));
+                }
+            });
+
+        let program_clone_response_error = Rc::clone(self);
+        let fail_closure: Closure<FnMut(JsValue)> =
+            Closure::once(move |js_value: JsValue| {
+                crate::log!("failed to get a response: {:#?}", js_value);
+                program_clone_response_error.dispatch(fail_cb(js_value));
+            });
+
+        promise.then(&cb).catch(&fail_closure);
+
+        cb.forget();
+        fail_closure.forget();
     }
 }
 
