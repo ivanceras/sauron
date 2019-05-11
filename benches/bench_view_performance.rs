b78f64c90011b20e316c3762dfd2fc740d920e7d
Jovansonlee Cesar <ivanceras@gmail.com>
2019-05-12 02:04:34 +0800

    Put benchmark inside black_box

--- benches/bench_view_performance.rs
+++ benches/bench_view_performance.rs
@@ -11,32 +11,33 @@ use sauron::{html::{attributes::*,
 #[bench]
 fn bench_view_builing_100_child_nodes(b: &mut Bencher) {
     b.iter(|| {
-         let view: Node<()> = div([class("some-class")],
-                                  (0..100).into_iter()
-                                          .map(|n| {
-                                              div([class("child-div")],
-                                                  [text(format!("node: {}",
-                                                                n))])
-                                          })
-                                          .collect::<Vec<Node<()>>>());
+         let view: Node<()> =
+             black_box(div([class("some-class")],
+                           (0..100).into_iter()
+                                   .map(|n| {
+                                       div([class("child-div")],
+                                           [text(format!("node: {}", n))])
+                                   })
+                                   .collect::<Vec<Node<()>>>()));
      });
 }
 
 #[bench]
 fn bench_view_builing_100_nodes_with_100_child_nodes(b: &mut Bencher) {
     b.iter(|| {
-         let view: Node<()> = div([class("some-class")],
-                                  (0..100).into_iter()
-                                          .map(|n| {
-                                              div([class("parent"), class(n)],
-                                 (0..100).into_iter()
-                                         .map(|n2| {
-                                             div([class("child-div")],
+         let view: Node<()> =
+             black_box(div([class("some-class")],
+                           (0..100).into_iter()
+                                   .map(|n| {
+                                       div([class("parent"), class(n)],
+                                           (0..100).into_iter()
+                                                   .map(|n2| {
+                                                       div([class("child-div")],
                                                  [text(format!("node: {}",
                                                                n2))])
-                                         })
-                                         .collect::<Vec<Node<()>>>())
-                                          })
-                                          .collect::<Vec<Node<()>>>());
+                                                   })
+                                                   .collect::<Vec<Node<()>>>())
+                                   })
+                                   .collect::<Vec<Node<()>>>()));
      });
 }
