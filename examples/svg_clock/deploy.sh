876f1e504b7607dc1a1ea04481af8559eb36c98e
ivanceras <ivanceras@gmail.com>
2019-11-16 15:15:48 +0800

    Add a deploy script for svg_clock

---
+++ examples/svg_clock/deploy.sh
@@ -0,0 +1,20 @@
+set -v
+
+if ! type wasm-pack > /dev/null; then
+    echo "wasm-pack is not installed"
+    cargo install wasm-pack
+fi
+
+if ! type basic-http-server > /dev/null; then
+    echo "basic-http-server is not installed"
+    cargo install basic-http-server
+fi
+
+wasm-pack build --target no-modules --release -- --features "wee_alloc"
+
+dest_dir="../../../ivanceras.github.io/svg_clock"
+
+mkdir -p $dest_dir;
+
+cp index.html $dest_dir/index.html
+cp -r pkg $dest_dir/svg_clock/
