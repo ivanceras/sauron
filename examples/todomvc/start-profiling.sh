b34a1c1848f4ba2aedfac5fd4a839ba5e956bf19
Jovansonlee Cesar <ivanceras@gmail.com>
2019-12-05 23:01:57 +0800

    Add the compiled code into 2 benchmark repo

--- examples/todomvc/start-profiling.sh
+++ examples/todomvc/start-profiling.sh
@@ -8,6 +8,10 @@ wasm-pack build --target no-modules --profiling --
 
 sleep 1
 
+mkdir -p $HOME/playground/mogwai-perf/todomvc/sauron/
+
 cp -r index.html style.css pkg ../../../todomvc-perf-comparison/todomvc-benchmark/todomvc/sauron/
+
+cp -r index.html style.css pkg $HOME/playground/mogwai-perf/todomvc/sauron/
 
 basic-http-server ./ -a 0.0.0.0:6001
--- examples/todomvc/start-release-mode-noweealloc.sh
+++ examples/todomvc/start-release-mode-noweealloc.sh
@@ -9,4 +9,6 @@ wasm-pack build --target no-modules --release
 
 cp -r index.html style.css pkg ../../../todomvc-perf-comparison/todomvc-benchmark/todomvc/sauron/
 
+cp -r index.html style.css pkg $HOME/playground/mogwai-perf/todomvc/sauron/
+
 basic-http-server ./ -a 0.0.0.0:6001
