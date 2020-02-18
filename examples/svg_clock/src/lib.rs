80cc42aec442da7b4125b56d8d1822f189e93bca
Jovansonlee Cesar <ivanceras@gmail.com>
2020-02-18 23:07:12 +0800

    Make the subsecond update to the subsecond by ticking at every 20ms

--- examples/svg_clock/src/lib.rs
+++ examples/svg_clock/src/lib.rs
@@ -53,10 +53,12 @@ impl Component<Msg> for Clock {
             []
         );
 
-        let hand = |rotate: f64,
-                    stroke_color: &'static str,
-                    stroke_width_value: u32,
-                    height: u32| {
+        let hand = |
+            rotate: f64,
+            stroke_color: &'static str,
+            stroke_width_value: u32,
+            height: u32,
+        | {
             svg::tags::line(
                 vec![
                     x1(100),
@@ -87,25 +89,36 @@ impl Component<Msg> for Clock {
         let hour_rotate =
             90.0 + ((ms / 1000.0 / 60.0 / 60.0) % 12.0) * 360.0 / 12.0;
 
-        article!([],[
-            h2!([],[text!("Sauron clock demonstrating svg dom manipulation")]),
-            a!([html::attributes::href("https://github.com/ivanceras/sauron/tree/master/examples/svg_clock")],[text!("code")]),
-            div!(
-                [style(
-                    "display: flex; align-items: center; flex-direction: column;"
-                )],
-                [svg!(
-                    [width(400), height(400), viewBox([0, 0, 200, 200])],
-                    [
-                        circle,
-                        hand(subsecond_rotate, "#e2e8f0", 10, 90),
-                        hand(hour_rotate, "#2d3748", 4, 50),
-                        hand(minute_rotate, "#2d3748", 3, 70),
-                        hand(second_rotate, "#e53e3e", 2, 90),
-                    ]
-                )]
-            )
-        ])
+        article!(
+            [],
+            [
+                h2!(
+                    [],
+                    [text!("Sauron clock demonstrating svg dom manipulation")]
+                ),
+                a!(
+                    [html::attributes::href(
+                        "https://github.com/ivanceras/sauron/tree/master/examples/svg_clock"
+                    )],
+                    [text!("code")]
+                ),
+                div!(
+                    [style(
+                        "display: flex; align-items: center; flex-direction: column;"
+                    )],
+                    [svg!(
+                        [width(400), height(400), viewBox([0, 0, 200, 200])],
+                        [
+                            circle,
+                            hand(subsecond_rotate, "#e2e8f0", 10, 90),
+                            hand(hour_rotate, "#2d3748", 4, 50),
+                            hand(minute_rotate, "#2d3748", 3, 70),
+                            hand(second_rotate, "#e53e3e", 2, 90),
+                        ]
+                    )]
+                )
+            ]
+        )
     }
 }
 
@@ -129,7 +142,7 @@ pub fn main() {
     window()
         .set_interval_with_callback_and_timeout_and_arguments_0(
             clock.as_ref().unchecked_ref(),
-            1000,
+            20,
         )
         .expect("Unable to start interval");
     clock.forget();
