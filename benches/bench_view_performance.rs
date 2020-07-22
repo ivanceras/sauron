2cb49ac221e6ec72df950c1d69d7afecbe8428df
Jovansonlee Cesar <ivanceras@gmail.com>
2020-07-23 01:56:07 +0800

    fix benchmark code

--- benches/bench_view_performance.rs
+++ benches/bench_view_performance.rs
@@ -1,58 +1,52 @@
-#![feature(test)]
-extern crate test;
-use test::{
-    black_box,
-    Bencher,
-};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use sauron::{
-    html::{
-        attributes::*,
-        *,
-    },
+    html::{attributes::*, *},
     Node,
 };
 
-#[bench]
-fn bench_view_builing_100_child_nodes(b: &mut Bencher) {
-    b.iter(|| {
-        let _view: Node<()> = black_box(div(
-            vec![class("some-class")],
-            (0..100)
-                .into_iter()
-                .map(|n| {
-                    div(
-                        vec![class("child-div")],
-                        vec![text(format!("node: {}", n))],
-                    )
-                })
-                .collect::<Vec<Node<()>>>(),
-        ));
+fn bench_view_builing_100_child_nodes(c: &mut Criterion) {
+    c.bench_function("100 nodes", |b| {
+        b.iter(|| {
+            let _view: Node<()> = black_box(div(
+                vec![class("some-class")],
+                (0..100)
+                    .into_iter()
+                    .map(|n| {
+                        div(
+                            vec![class("child-div")],
+                            vec![text(format!("node: {}", n))],
+                        )
+                    })
+                    .collect::<Vec<Node<()>>>(),
+            ));
+        })
     });
 }
 
-#[bench]
-fn bench_view_builing_100_nodes_with_100_child_nodes(b: &mut Bencher) {
-    b.iter(|| {
-        let _view: Node<()> = black_box(div(
-            vec![class("some-class")],
-            (0..100)
-                .into_iter()
-                .map(|n| {
-                    div(
-                        vec![class("parent"), class(n)],
-                        (0..100)
-                            .into_iter()
-                            .map(|n2| {
-                                div(
-                                    vec![class("child-div")],
-                                    vec![text(format!("node: {}", n2))],
-                                )
-                            })
-                            .collect::<Vec<Node<()>>>(),
-                    )
-                })
-                .collect::<Vec<Node<()>>>(),
-        ));
+fn bench_view_builing_100_nodes_with_100_child_nodes(c: &mut Criterion) {
+    c.bench_function("100 100", |b| {
+        b.iter(|| {
+            let _view: Node<()> = black_box(div(
+                vec![class("some-class")],
+                (0..100)
+                    .into_iter()
+                    .map(|n| {
+                        div(
+                            vec![class("parent"), class(n)],
+                            (0..100)
+                                .into_iter()
+                                .map(|n2| {
+                                    div(
+                                        vec![class("child-div")],
+                                        vec![text(format!("node: {}", n2))],
+                                    )
+                                })
+                                .collect::<Vec<Node<()>>>(),
+                        )
+                    })
+                    .collect::<Vec<Node<()>>>(),
+            ));
+        })
     });
 }
