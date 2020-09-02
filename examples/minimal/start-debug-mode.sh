0a9e8890c34459d4cc3453870986b982c4cc44e0
Jovansonlee Cesar <ivanceras@gmail.com>
2020-09-02 10:58:12 +0800

    fix minimal exampel for webkit base browser

--- examples/minimal/start-debug-mode.sh
+++ examples/minimal/start-debug-mode.sh
@@ -5,6 +5,6 @@ set -v
 . ./bootstrap.sh
 
 
-wasm-pack build --target no-modules --dev -- --features "console_error_panic_hook with-measure"
+wasm-pack build --target web --dev -- --features "with-measure"
 
 basic-http-server ./ -a 0.0.0.0:4001
