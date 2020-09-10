5919bbf924d87d564a86c8b0c0b3ba2b2c68b23b
Jovansonlee Cesar <ivanceras@gmail.com>
2020-09-10 08:54:09 +0800

    Add tests on sauron-node-macro

---
+++ crates/sauron-node-macro/tests/style_in_node.rs
@@ -0,0 +1,10 @@
+use sauron::{node, Node, Render};
+
+#[test]
+fn style_should_be_valid() {
+    let node1: Node<()> = node!(<div style="border:1px solid green;"></div>);
+    let expected = r#"<div style="border:1px solid green;"></div>"#;
+    let mut buffer = String::new();
+    node1.render(&mut buffer).expect("must have no error");
+    assert_eq!(expected, buffer);
+}
