c9535de7f899dbec6aa18e8a7ca0166b5953d2b1
Jovansonlee Cesar <ivanceras@gmail.com>
2021-08-13 18:12:51 +0800

    Refactor Http fetch api to move the control of response decoding at the calling function

--- crates/sauron-core/src/dom/http.rs
+++ crates/sauron-core/src/dom/http.rs
@@ -1,10 +1,9 @@
 //! provides functions for retrieving data using http network request
 use crate::html::attributes::Callback;
-use crate::{Cmd, Component, Dispatch};
+use crate::{Cmd, Component, Dispatch, Program};
 use js_sys::TypeError;
 use std::fmt::Debug;
 use wasm_bindgen::{closure::Closure, JsCast, JsValue};
-use web_sys::Headers;
 use web_sys::RequestInit;
 use web_sys::Response;
 
@@ -27,44 +26,57 @@ impl Http {
         MSG: 'static,
     {
         let cb = Callback::from(cb);
-        let text_response_decoder =
-            move |(_status_code, response_text, _headers): (
-                u16,
-                String,
-                Headers,
-            )| { cb.emit(response_text) };
+
+        let response_extractor =
+            move |(program, response): (Program<APP, MSG>, Response)| {
+                let response_promise =
+                    response.text().expect("must be a promise text");
+
+                let cb = cb.clone();
+
+                let decoder_and_dispatcher_cb: Closure<dyn FnMut(JsValue)> =
+                    Closure::once(move |js_value: JsValue| {
+                        let response_text = js_value
+                            .as_string()
+                            .expect("There's no string value");
+                        let msg = cb.emit(response_text);
+                        program.dispatch(msg);
+                    });
+
+                let _ = response_promise.then(&decoder_and_dispatcher_cb);
+
+                decoder_and_dispatcher_cb.forget();
+            };
+
         let err_cb = move |type_error| err_cb(type_error);
         Self::fetch_with_request_and_response_decoder(
             url,
             None,
-            text_response_decoder,
+            response_extractor,
             err_cb,
         )
     }
 
     /// API for fetching http rest request
     /// err_cb - request failed, in cases where a network is down, server is dead, etc.
-    pub fn fetch_with_request_and_response_decoder<APP, MSG, DECODE, ERR>(
+    pub fn fetch_with_request_and_response_decoder<APP, MSG, EXTRACTOR, ERR>(
         url: &str,
         request_init: Option<RequestInit>,
-        text_response_decoder: DECODE,
+        response_extractor: EXTRACTOR,
         err_cb: ERR,
     ) -> Cmd<APP, MSG>
     where
         APP: Component<MSG> + 'static,
         MSG: 'static,
-        DECODE: Fn((u16, String, Headers)) -> MSG + 'static,
+        EXTRACTOR: Fn((Program<APP, MSG>, Response)) + 'static,
         ERR: Fn(TypeError) -> MSG + 'static,
     {
         let url_clone = url.to_string();
-        let text_response_decoder = Callback::from(text_response_decoder);
 
         let err_cb = Callback::from(err_cb);
+        let response_extractor_cb = Callback::from(response_extractor);
         Cmd::new(move |program| {
             let program_clone = program.clone();
-
-            let text_response_decoder = text_response_decoder.clone();
-
             let err_cb = err_cb.clone();
 
             let window =
@@ -76,28 +88,11 @@ impl Http {
                 window.fetch_with_str(&url_clone)
             };
 
+            let response_extractor_cb_clone = response_extractor_cb.clone();
             let cb: Closure<dyn FnMut(JsValue)> =
                 Closure::once(move |js_value: JsValue| {
                     let response: Response = js_value.unchecked_into();
-                    let status = response.status();
-                    let response_promise =
-                        response.text().expect("must be a promise text");
-                    let decoder_and_dispatcher_cb: Closure<dyn FnMut(JsValue)> =
-                        Closure::once(move |js_value: JsValue| {
-                            let response_text = js_value
-                                .as_string()
-                                .expect("There's no string value");
-                            let msg = text_response_decoder.emit((
-                                status,
-                                response_text,
-                                response.headers(),
-                            ));
-                            program_clone.dispatch(msg);
-                        });
-
-                    let _ = response_promise.then(&decoder_and_dispatcher_cb);
-
-                    decoder_and_dispatcher_cb.forget();
+                    response_extractor_cb_clone.emit((program_clone, response));
                 });
 
             let err_closure: Closure<dyn FnMut(JsValue)> =
