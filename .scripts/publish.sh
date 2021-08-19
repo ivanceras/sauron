6d4d9cd86cd31d4ee410095b33f65874c4422f99
Jovansonlee Cesar <ivanceras@gmail.com>
2021-08-20 02:53:17 +0800

    Add a publish script to automate the process

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -2,5 +2,5 @@
 
 set -ev
 cd crates/sauron-core && cargo publish && cd - && \
-cd crates/sauron-node-macro && cargo publish && cd - &&
+cd crates/sauron-node-macro && sleep 5 && cargo publish && cd - &&
 cargo publish
