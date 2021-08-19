6c6e7b515b270fd56fe145396106d6997960abd3
Jovansonlee Cesar <ivanceras@gmail.com>
2021-08-20 02:56:52 +0800

    Improve the publish script

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# script to publish the crates 
+# we added a 5s sleep in between publish to give time for dependency crate to propagate to crates.io
+#
+
 set -ev
 cd crates/sauron-core && cargo publish && cd - && \
-cd crates/sauron-node-macro && sleep 5 && cargo publish && cd - &&
+echo "sleeping for 5s" && sleep 5 &&\
+cd crates/sauron-node-macro && cargo publish && cd - &&
+echo "sleeping for 5s" && sleep 5 &&\
 cargo publish
