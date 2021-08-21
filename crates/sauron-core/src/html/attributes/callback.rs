803edf0a4622bdeccf69f6ae8a1d8153f8598e4c
Jovansonlee Cesar <ivanceras@gmail.com>
2021-08-21 14:47:05 +0800

    Rename generics in Callback<EVENT,MSG> to Callback<IN,OUT> for a more generic application

--- crates/sauron-core/src/html/attributes/callback.rs
+++ crates/sauron-core/src/html/attributes/callback.rs
@@ -11,10 +11,10 @@ use std::{convert::Into, fmt, rc::Rc};
 /// therefore it can not mutate the environment variables
 ///
 /// In effect callbacks attached to DOM events are limited
-/// to only passing an MSG to the program and not complex statements.
+/// to only passing an OUT to the program and not complex statements.
 ///
 /// Note: It would have been nice to have the inner value be
-/// `Rc<FnMut(EVENT) -> MSG> +'a`
+/// `Rc<FnMut(IN) -> OUT> +'a`
 /// but there are a lot of issues that this becomes infeasible:
 ///  1 - wasm_bindgen::Closure requires that 'static references to the closure
 ///  2 - Accessing `Rc::get_mut` requires that there is no other `Rc` or `Weak` references
@@ -24,57 +24,57 @@ use std::{convert::Into, fmt, rc::Rc};
 ///         - Attributes of the same name are merged therefore cloning the attributes, hence the
 ///         callback is necessary.
 ///
-pub struct Callback<EVENT, MSG> {
+pub struct Callback<IN, OUT> {
     /// the function to be executed
-    func: Rc<dyn Fn(EVENT) -> MSG>,
+    func: Rc<dyn Fn(IN) -> OUT>,
     /// the type type_id of the event this callback will be attached to
     event_type_id: TypeId,
     /// the type_id of the return type of this callback when executed.
     msg_type_id: TypeId,
 }
 
-impl<EVENT, F, MSG> From<F> for Callback<EVENT, MSG>
+impl<IN, F, OUT> From<F> for Callback<IN, OUT>
 where
-    F: Fn(EVENT) -> MSG + 'static,
-    MSG: 'static,
-    EVENT: 'static,
+    F: Fn(IN) -> OUT + 'static,
+    OUT: 'static,
+    IN: 'static,
 {
     fn from(func: F) -> Self {
         Self {
             func: Rc::new(func),
-            event_type_id: TypeId::of::<EVENT>(),
-            msg_type_id: TypeId::of::<MSG>(),
+            event_type_id: TypeId::of::<IN>(),
+            msg_type_id: TypeId::of::<OUT>(),
         }
     }
 }
 
 /// Note:
-/// using the #[derive(Debug)] needs EVENT and MSG to also be Debug
+/// using the #[derive(Debug)] needs IN and OUT to also be Debug
 ///
-/// The reason this is manually implemented is, so that EVENT and MSG
+/// The reason this is manually implemented is, so that IN and OUT
 /// doesn't need to be Debug as it is part of the Callback objects and are not shown.
-impl<EVENT, MSG> fmt::Debug for Callback<EVENT, MSG> {
+impl<IN, OUT> fmt::Debug for Callback<IN, OUT> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "||{{..}}")
     }
 }
 
-impl<EVENT, MSG> Callback<EVENT, MSG>
+impl<IN, OUT> Callback<IN, OUT>
 where
-    EVENT: 'static,
-    MSG: 'static,
+    IN: 'static,
+    OUT: 'static,
 {
     /// This method calls the actual callback.
-    pub fn emit<T: Into<EVENT>>(&self, value: T) -> MSG {
+    pub fn emit<T: Into<IN>>(&self, value: T) -> OUT {
         (self.func)(value.into())
     }
 
     /// map this callback using another callback such that
-    /// MSG becomes MSG2
+    /// OUT becomes MSG2
     pub fn map_callback<MSG2>(
         self,
-        cb: Callback<MSG, MSG2>,
-    ) -> Callback<EVENT, MSG2>
+        cb: Callback<OUT, MSG2>,
+    ) -> Callback<IN, MSG2>
     where
         MSG2: 'static,
     {
@@ -87,12 +87,12 @@ where
 }
 
 /// Note:
-/// using the #[derive(Clone)] needs EVENT and MSG to also be Clone
+/// using the #[derive(Clone)] needs IN and OUT to also be Clone
 ///
-/// The reason this is manually implemented is, so that EVENT and MSG
+/// The reason this is manually implemented is, so that IN and OUT
 /// doesn't need to be Clone as it is part of the Callback objects and cloning here
 /// is just cloning the pointer of the actual callback function
-impl<EVENT, MSG> Clone for Callback<EVENT, MSG> {
+impl<IN, OUT> Clone for Callback<IN, OUT> {
     fn clone(&self) -> Self {
         Self {
             func: Rc::clone(&self.func),
@@ -109,7 +109,7 @@ impl<EVENT, MSG> Clone for Callback<EVENT, MSG> {
 ///
 /// This is done by comparing the type_id of the input and type_id of the output.
 ///
-impl<EVENT, MSG> PartialEq for Callback<EVENT, MSG> {
+impl<IN, OUT> PartialEq for Callback<IN, OUT> {
     fn eq(&self, other: &Self) -> bool {
         self.event_type_id == other.event_type_id
             && self.msg_type_id == other.msg_type_id
