be2f788bf2ac7919268874122144803198553b1e
Jovansonlee Cesar <ivanceras@gmail.com>
2022-02-24 06:50:38 +0800

    fix imports for jss

--- src/lib.rs
+++ src/lib.rs
@@ -34,8 +34,9 @@ pub mod prelude {
     pub use sauron_node_macro::node;
 }
 pub use sauron_core::{
-    html, jss::jss, jss::jss_ns, mt_dom, svg, vdom::diff, vdom::Attribute,
-    vdom::Element, vdom::Listener, vdom::Node, vdom::Patch, Render,
+    html, jss::jss, jss::jss_ns, jss::units, mt_dom, svg, vdom::diff,
+    vdom::Attribute, vdom::Element, vdom::Listener, vdom::Node, vdom::Patch,
+    Render,
 };
 #[cfg(feature = "with-node-macro")]
 pub use sauron_node_macro::node;
