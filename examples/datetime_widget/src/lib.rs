0e31e6144d819dc958f82332ef24798389e04055
Jovansonlee Cesar <ivanceras@gmail.com>
2022-05-19 02:11:54 +0800

    feat: improve the custom date-time widget example

--- examples/datetime_widget/src/lib.rs
+++ examples/datetime_widget/src/lib.rs
@@ -8,12 +8,12 @@ use std::collections::BTreeMap;
 mod date_time;
 
 #[derive(Debug, Clone)]
-pub struct Msg(date_time::Msg);
+pub struct CustomMsg(date_time::Msg);
 
 #[wasm_bindgen]
 pub struct DateTimeWidgetCustomElement {
     is_mounted: bool,
-    program: Program<DateTimeWidget<Msg>, Msg>,
+    program: Program<DateTimeWidget<CustomMsg>, CustomMsg>,
 }
 
 #[wasm_bindgen]
@@ -24,16 +24,13 @@ impl DateTimeWidgetCustomElement {
         let root_node: &web_sys::Node = node.unchecked_ref();
         Self {
             is_mounted: false,
-            program: Program::append_to_mount(
-                DateTimeWidget::default(),
-                root_node,
-            ),
+            program: Program::new(DateTimeWidget::default(), root_node),
         }
     }
 
     #[wasm_bindgen(method)]
     pub fn observed_attributes() -> JsValue {
-        JsValue::from_serde(&DateTimeWidget::<Msg>::observed_attributes())
+        JsValue::from_serde(&DateTimeWidget::<CustomMsg>::observed_attributes())
             .expect("must parse from serde")
     }
 
@@ -57,15 +54,14 @@ impl DateTimeWidgetCustomElement {
             .app
             .borrow_mut()
             .attribute_changed(attribute_values);
-        self.program.update_dom();
     }
 
     #[wasm_bindgen(method)]
     pub fn connected_callback(&mut self) {
+        self.program.start_append_to_mount();
         self.is_mounted = true;
         log::info!("Component is connected..");
-        //self.attribute_changed_callback();
-        //self.program.start_append_to_mount();
+        self.program.update_dom();
     }
     #[wasm_bindgen(method)]
     pub fn disconnected_callback(&mut self) {
@@ -79,16 +75,17 @@ impl DateTimeWidgetCustomElement {
     }
 }
 
-impl Application<Msg> for DateTimeWidget<Msg> {
-    fn update(&mut self, msg: Msg) -> Cmd<Self, Msg> {
+impl Application<CustomMsg> for DateTimeWidget<CustomMsg> {
+    fn update(&mut self, msg: CustomMsg) -> Cmd<Self, CustomMsg> {
         Cmd::from(
-            <Self as Component<date_time::Msg, Msg>>::update(self, msg.0)
-                .localize(Msg),
+            <Self as Component<date_time::Msg, CustomMsg>>::update(self, msg.0)
+                .localize(CustomMsg),
         )
     }
 
-    fn view(&self) -> Node<Msg> {
-        <Self as Component<date_time::Msg, Msg>>::view(self).map_msg(Msg)
+    fn view(&self) -> Node<CustomMsg> {
+        <Self as Component<date_time::Msg, CustomMsg>>::view(self)
+            .map_msg(CustomMsg)
     }
 }
 
@@ -97,10 +94,38 @@ extern "C" {
     pub fn register_custom_element(custom_tag: &str, adapter: &str);
 }
 
+#[wasm_bindgen]
+pub fn register_components() {
+    register_custom_element("date-time", "DateTimeWidgetCustomElement");
+}
+
+pub enum AppMsg {
+    Nothing,
+}
+
+#[derive(Default)]
+pub struct App {}
+
+impl Application<AppMsg> for App {
+    fn update(&mut self, _msg: AppMsg) -> Cmd<Self, AppMsg> {
+        Cmd::none()
+    }
+
+    fn view(&self) -> Node<AppMsg> {
+        node! {
+            <div>
+                <h5>"Usage of custom element"</h5>
+                <date-time date="2022-05-16" time="15:46"></date-time>
+            </div>
+        }
+    }
+}
+
 #[wasm_bindgen(start)]
 pub fn main() {
     console_log::init_with_level(log::Level::Trace).unwrap();
     console_error_panic_hook::set_once();
-    register_custom_element("date-time", "DateTimeWidgetCustomElement");
     log::info!("loaded...");
+    register_components();
+    Program::mount_to_body(App::default());
 }
