07ecd485be94e993454f5a6e8f90d487d8b6a0e6
Jovansonlee Cesar <ivanceras@gmail.com>
2022-05-22 04:22:36 +0800

    fix: extern block don't generate rustdocs

--- crates/sauron-core/src/dom/custom_element.rs
+++ crates/sauron-core/src/dom/custom_element.rs
@@ -2,11 +2,11 @@ use crate::prelude::*;
 
 #[wasm_bindgen(module = "/js/define_custom_element.js")]
 extern "C" {
-    /// register using custom element define
-    /// # Example:
-    /// ```rust,ignore
-    ///  sauron::register_custom_element("date-time", "DateTimeWidgetCustomElement", "HTMLElement");
-    /// ```
+    // register using custom element define
+    // # Example:
+    // ```rust,ignore
+    //  sauron::register_custom_element("date-time", "DateTimeWidgetCustomElement", "HTMLElement");
+    // ```
     pub fn register_custom_element(
         custom_tag: &str,
         adapter: &str,
