468391d6372c5fa5929fa5d0544771777baa0dd4
Jovansonlee Cesar <ivanceras@gmail.com>
2022-09-03 21:52:05 +0800

    refactor: assign the components into the window object, rather than in window.components

--- crates/sauron-core/js/define_custom_element.js
+++ crates/sauron-core/js/define_custom_element.js
@@ -9,29 +9,29 @@
 export function register_custom_element(custom_tag, adapter, superClass){
     // https://developer.mozilla.org/en-US/docs/Web/API/Window/customElements
     window.customElements.define(custom_tag,
-                class extends window[superClass]{
-                    constructor(){
-                        super();
-                        this.instance = new window.components[adapter](this);
-                    }
+        class extends window[superClass]{
+            constructor(){
+                super();
+                this.instance = new window[adapter](this);
+            }
 
-                    static get observedAttributes(){
-                        return window.components[adapter].observed_attributes();
-                    }
+            static get observedAttributes(){
+                return window[adapter].observed_attributes();
+            }
 
-                    connectedCallback(){
-                        this.instance.connected_callback();
-                    }
-                    disconnectedCallback(){
-                        this.instance.disconnected_callback();
-                    }
-                    adoptedCallback(){
-                        this.instance.adopted_callback();
-                    }
-                    attributeChangedCallback(){
-                        this.instance.attribute_changed_callback();
-                    }
+            connectedCallback(){
+                this.instance.connected_callback();
+            }
+            disconnectedCallback(){
+                this.instance.disconnected_callback();
+            }
+            adoptedCallback(){
+                this.instance.adopted_callback();
+            }
+            attributeChangedCallback(){
+                this.instance.attribute_changed_callback();
+            }
 
-                }
-            );
-    }
+        }
+    );
+}
