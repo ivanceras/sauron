b6df8b454ccaa5a37b3ad23e9b37cfa06b47512a
Jovansonlee Cesar <ivanceras@gmail.com>
2023-04-15 12:11:12 +0800

    fix: publishing script, no need to manually sleep in order to wait for crates.io to propagate the publishing, as it is done in crates.io already

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 # script to publish the crates 
-# we added a 5s sleep in between publish to give time for dependency crate to propagate to crates.io
-#
 
 set -ev
 cd crates/sauron-core && cargo publish && cd - && \
-echo "sleeping" && sleep 20 &&\
 cd crates/sauron-node-macro && cargo publish && cd - &&
-echo "sleeping" && sleep 20 &&\
 cargo publish
