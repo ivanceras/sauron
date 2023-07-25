6b863314988174587376d775488811189c03238e
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-25 16:57:40 +0800

    add more test when swapping rows

--- tests/swap_nodes.rs
+++ tests/swap_nodes.rs
@@ -129,3 +129,72 @@ fn swap_rows_keyed() {
 
     assert_eq!(expected1, container.outer_html());
 }
+
+#[wasm_bindgen_test]
+fn swap_1_and_8() {
+    console_log::init_with_level(log::Level::Trace).ok();
+    console_error_panic_hook::set_once();
+
+    let document = web_sys::window().unwrap().document().unwrap();
+    let class_name = "swap_8th";
+
+    let old: Node<()> = main(
+        vec![class(class_name)],
+        vec![ul(
+            vec![class("todo")],
+            vec![
+                li(vec![key(0)], vec![text("item0")]),
+                li(vec![key(1)], vec![text("item1")]),
+                li(vec![key(2)], vec![text("item2")]),
+                li(vec![key(3)], vec![text("item3")]),
+                li(vec![key(4)], vec![text("item4")]),
+                li(vec![key(5)], vec![text("item5")]),
+                li(vec![key(6)], vec![text("item6")]),
+                li(vec![key(7)], vec![text("item7")]),
+                li(vec![key(8)], vec![text("item8")]),
+                li(vec![key(9)], vec![text("item9")]),
+            ],
+        )],
+    );
+
+    let update1: Node<()> = main(
+        vec![class(class_name)],
+        vec![ul(
+            vec![class("todo")],
+            vec![
+                li(vec![key(0)], vec![text("item0")]),
+                li(vec![key(8)], vec![text("item8")]),
+                li(vec![key(2)], vec![text("item2")]),
+                li(vec![key(3)], vec![text("item3")]),
+                li(vec![key(4)], vec![text("item4")]),
+                li(vec![key(5)], vec![text("item5")]),
+                li(vec![key(6)], vec![text("item6")]),
+                li(vec![key(7)], vec![text("item7")]),
+                li(vec![key(1)], vec![text("item1")]),
+                li(vec![key(9)], vec![text("item9")]),
+            ],
+        )],
+    );
+
+    let patches = diff(&old, &update1);
+    log::debug!("patches: {:#?}", patches);
+
+    let mut old_html = String::new();
+    old.render(&mut old_html).expect("must render");
+
+    let simple_program = simple_program();
+    simple_program.set_current_dom(old);
+
+    let expected1 = update1.render_to_string();
+
+    simple_program
+        .update_dom_with_vdom(update1)
+        .expect("must not error");
+
+    let container = document
+        .query_selector(&format!(".{class_name}"))
+        .expect("must not error")
+        .expect("must exist");
+
+    assert_eq!(expected1, container.outer_html());
+}
