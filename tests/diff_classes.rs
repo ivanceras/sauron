655e7d97219b133c1812fcad40cec760566bd4d6
Jovansonlee Cesar <ivanceras@gmail.com>
2023-08-16 21:17:27 +0800

    add a test-case for issue#75

---
+++ tests/diff_classes.rs
@@ -0,0 +1,48 @@
+#![deny(warnings)]
+use sauron::*;
+
+#[test]
+fn class_with_bool_value() {
+    let old: Node<()> = div(vec![class(false)], vec![]);
+
+    let new = div(vec![class(true)], vec![]);
+    assert_eq!(
+        diff(&old, &new),
+        vec![Patch::add_attributes(
+            &"div",
+            TreePath::new(vec![]),
+            vec![&class(true)]
+        )],
+        "Should add the new attributes"
+    );
+}
+
+#[test]
+fn parent_of_matching_keyed_are_ignored() {
+    let old: Node<()> = ul(
+        [class("original")],
+        [
+            li([key("0")], [text("text0")]),
+            li([key("1")], [text("text1")]),
+            li([key("2")], [text("text2")]),
+        ],
+    );
+
+    let new = ul(
+        [class("changed")],
+        [
+            li([key("0")], [text("text0")]),
+            li([key("1")], [text("text1")]),
+            li([key("2")], [text("text2")]),
+        ],
+    );
+    assert_eq!(
+        diff(&old, &new),
+        vec![Patch::add_attributes(
+            &"ul",
+            TreePath::new(vec![]),
+            vec![&class("changed")]
+        )],
+        "Should add the new attributes"
+    );
+}
