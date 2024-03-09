471038f8e16e7d29565d731cee525ef75322b829
Jovansonlee Cesar <ivanceras@gmail.com>
2024-03-09 15:00:26 +0800

    fix: remove assertions in counting references

--- crates/core/src/dom/program/app_context.rs
+++ crates/core/src/dom/program/app_context.rs
@@ -98,16 +98,10 @@ where
         }
     }
     pub fn strong_count(&self) -> usize {
-        let c1 = Rc::strong_count(&self.app);
-        let c2 = Rc::strong_count(&self.current_vdom);
-        assert_eq!(c1, c2);
-        c1
+        Rc::strong_count(&self.app)
     }
     pub fn weak_count(&self) -> usize {
-        let w1 = Rc::weak_count(&self.app);
-        let w2 = Rc::weak_count(&self.current_vdom);
-        assert_eq!(w1, w2);
-        w1
+        Rc::weak_count(&self.app)
     }
 }
 
