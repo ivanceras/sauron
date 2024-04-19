50616a64062fd25e3ec85b7c774c54f595aa1c49
Jovansonlee Cesar <ivanceras@gmail.com>
2024-04-20 00:36:41 +0800

    fix: typo

--- crates/core/src/dom/component/stateful_component.rs
+++ crates/core/src/dom/component/stateful_component.rs
@@ -49,7 +49,7 @@ pub struct StatefulModel<MSG> {
 
 impl<MSG> fmt::Debug for StatefulModel<MSG> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "StatefuleMode")
+        write!(f, "StatefulModel")
     }
 }
 
